e4db7b55534825dbc530dfd7c5b421addd8c8b3c
Gal Pressman <galpressman@gmail.com>
2016-05-23 15:42:39 +0000

    Add timer and remove README
    Signed-off-by: Gal Pressman <galpressman@gmail.com>

--- src/views/basic_views.py
+++ src/views/basic_views.py
@@ -24,11 +24,21 @@ class BasicViews():
             ('.*(overwatch)|(OVERWATCH)|(Overwatch).*$', self.overwatch_hype),
         ]
 
+        # spam something every timeout
+        threading.Timer(1, self.timer_callback, args=[900]).start()
+
     def send_text(self, data, to):
         self.interface_layer.toLower(TextMessageProtocolEntity(data, to=to))
 
     def send_image(self, url, to):
         self.media_views.image_sender.send_by_url(jid=to, file_url=url)
+
+    def timer_callback(self, interval):
+        MESSAGE = '''OVERWATCH BEGINS IN: %s.
+                     DON\'T BE LATE!''' % get_countdown(2016, 05, 24, 02)
+
+        self.send_text(MESSAGE, '972503305550-1369598727@g.us')
+        threading.Timer(interval, self.timer_callback).start()
 
     def echo(self, message, match):
         self.send_text(match.group("echo_message"), message.getFrom())
